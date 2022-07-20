57ef76887b14b4fe4e18ce253fdcc06ee482c5c5
Johannes van-den-Heuvel <vandenheu@cgpool1910.Informatik.Uni-Tuebingen.De>
2022-07-20 15:57:48 +0200

    added smoothing

--- lds.py
+++ lds.py
@@ -29,16 +29,39 @@ def info_kalman_filter(init_params, pair_params, observations):
     J, h = init_params
     J11, J12, J22 = pair_params
 
-    saved = []
+    forward_messages = []
     for (J_obs, h_obs) in observations:
         J_cond, h_cond = info_condition(J, h, J_obs, h_obs)
         J, h = info_predict(J_cond, h_cond, J11, J12, J22)
+        forward_messages.append(((J_cond, h_cond), (J, h)))
 
-        loc, scale = info_to_standard(J, h)
-        x = loc + scale @ torch.randn(1)
-        saved.append(x.detach().numpy())
+    return forward_messages
 
-    return saved
+
+def info_rst_smoothing(J, h, cond_msg, pred_msg, pair_params):
+    J_cond, h_cond = cond_msg
+    J_pred, h_pred = pred_msg
+    J11, J12, J22 = pair_params
+
+    temp = J12 @ torch.inverse(J - J_pred + J22)
+    J_smooth = J_cond + J11 - temp @ J12.T
+    # h_smooth = h_cond + h1 - temp @ (h - h_pred + h2)
+    h_smooth = h_cond - temp @ (h - h_pred)
+
+    return J_smooth, h_smooth
+
+
+def info_kalman_smoothing(forward_messages, pair_params):
+    _, (J_smooth, h_smooth) = forward_messages[-1]
+
+    backward_messages = [(J_smooth, h_smooth)]
+    for (cond_msg, pred_msg) in reversed(forward_messages[:-1]):
+        J_smooth, h_smooth = info_rst_smoothing(
+            J_smooth, h_smooth, cond_msg, pred_msg, pair_params
+        )
+        backward_messages.append((J_smooth, h_smooth))
+
+    return list(reversed(backward_messages))
 
 
 def info_observation_params(obs, C, R):
@@ -59,3 +82,21 @@ def info_pair_params(A, Q):
     J12 = -A.T @ J22
     J11 = A.T @ -J12
     return J11, J12, J22
+
+
+def sample_forward_messages(messages):
+    samples = []
+    for _, (J, h) in messages:
+        loc, scale = info_to_standard(J, h)
+        x = loc + scale @ torch.randn(1)
+        samples.append(x.detach().numpy())
+    return samples
+
+
+def sample_backward_messages(messages):
+    samples = []
+    for (J, h) in messages:
+        loc, scale = info_to_standard(J, h)
+        x = loc + scale @ torch.randn(1)
+        samples.append(x.detach().numpy())
+    return samples
--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import torch
 import matplotlib.pyplot as plt
 
 from data import generate_data
-from lds import info_pair_params, info_observation_params, info_kalman_filter
+from lds import (
+    info_pair_params,
+    info_observation_params,
+    info_kalman_filter,
+    info_kalman_smoothing,
+    sample_forward_messages,
+    sample_backward_messages,
+)
 
 if __name__ == "__main__":
     latents, obs = generate_data(100, noise_scale=1)
@@ -23,12 +30,20 @@ if __name__ == "__main__":
 
     init_params = (torch.inverse(Q), torch.zeros(1))
 
-    saved = info_kalman_filter(
+    forward_messages = info_kalman_filter(
         init_params=init_params, pair_params=(J11, J12, J22), observations=y
     )
+    backward_messages = info_kalman_smoothing(
+        forward_messages, pair_params=(J11, J12, J22)
+    )
 
-    plt.plot(latents.detach().numpy())
-    plt.plot(saved)
+    forward_samples = sample_forward_messages(forward_messages)
+    backward_samples = sample_backward_messages(backward_messages)
+
+    plt.plot(latents.detach().numpy(), label="observed")
+    plt.plot(forward_samples, label="predicted")
+    plt.plot(backward_samples, label="smoothed")
+    plt.legend()
     plt.xlabel("x1")
     plt.ylabel("x2")
     plt.show()
